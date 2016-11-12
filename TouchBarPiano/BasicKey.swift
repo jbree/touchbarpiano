0986596e64c3d55d0e756d007556d1736056c5e2
Joshua Breeden <jbreeden@me.com>
2016-11-11 22:27:52 -0500

    Add BasicKey and ControlViewController

---
+++ TouchBarPiano/BasicKey.swift
@@ -0,0 +1,19 @@
+//
+//  BasicKey.swift
+//  TouchBarPiano
+//
+//  Created by Joshua Breeden on 11/11/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+import Cocoa
+
+class BasicKey: NSView {
+
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+
+        // Drawing code here.
+    }
+    
+}
---
+++ TouchBarPiano/ControlViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ControlViewController.swift
+//  TouchBarPiano
+//
+//  Created by Joshua Breeden on 11/11/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+import Cocoa
+
+class ControlViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
