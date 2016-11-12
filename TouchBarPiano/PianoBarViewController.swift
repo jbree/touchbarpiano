2e1839c28742485f010f7fff5df49577c19733bc
Joshua Breeden <jbreeden@me.com>
2016-11-12 13:52:56 -0500

    Replace PianoBarController with PianoBarViewController

---
+++ TouchBarPiano/PianoBarViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PianoBarViewController.swift
+//  TouchBarPiano
+//
+//  Created by Joshua Breeden on 11/12/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+import Cocoa
+
+class PianoBarViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do view setup here.
+    }
+    
+}
