963c242a41bcc9bb678c083bec6353bba39131d5
Joshua Breeden <jbreeden@me.com>
2016-11-16 21:34:22 -0500

    add FirstResponderView

---
+++ TouchBarPiano/FirstResponderView.swift
@@ -0,0 +1,15 @@
+//
+//  FirstResponderView.swift
+//  TouchBarPiano
+//
+//  Created by Joshua Breeden on 11/16/16.
+//  Copyright © 2016 Joshua Breeden. All rights reserved.
+//
+
+import Cocoa
+
+class FirstResponderView: NSView {
+
+    override var acceptsFirstResponder: Bool { return true }
+    
+}
